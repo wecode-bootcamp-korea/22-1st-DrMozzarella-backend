14d5414a1610ffe4499b87ccee6d56ed3f5b759c
Dongmyeong Lee <dom9316@gmail.com>
2021-07-06 21:27:57 +0900

    Fix: add attribute to CategoryEvent

--- home/models.py
+++ home/models.py
@@ -3,14 +3,17 @@ from django.db import models
 class ProductEvent(models.Model):
     image_url  = models.URLField()
     product    = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         db_table = 'product_events'
 
 class CategoryEvent(models.Model):
-    image_url = models.URLField()
-    category  = models.ForeignKey('products.Category', on_delete=models.CASCADE)
+    image_url   = models.URLField()
+    category    = models.ForeignKey('products.Category', on_delete=models.CASCADE)
+    title       = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at  = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'category_events'
