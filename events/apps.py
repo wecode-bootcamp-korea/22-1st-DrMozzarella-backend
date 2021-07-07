c095f8351df5b9103a8f1df59160296deab1ffba
heesu-ahn <anhesu4@gmail.com>
2021-07-07 18:56:42 +0900

    test

--- events/apps.py
+++ events/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class EventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'events'
