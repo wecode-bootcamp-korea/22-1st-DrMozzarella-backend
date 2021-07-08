a5021cb9e9d307ce9983f35aea9a959b4fd3847e
Dongmyeong Lee <dom9316@gmail.com>
2021-07-08 16:25:12 +0900

    Add: meta endpoint for navigation bar

--- drmozzarella/urls.py
+++ drmozzarella/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
 
+from products.views import MetaView
+
 urlpatterns = [
+    path('meta', MetaView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http  import JsonResponse
+from django.views import View
 
-# Create your views here.
+from products.models import Menu
+
+class MetaView(View):
+    def get(self, request):
+        menus = Menu.objects.all()
+        
+        results = {}
+        for menu in menus:
+            results[menu.name] = {
+                "id": menu.id,
+                "categories": [
+                    {
+                        "id": category.id,
+                        "name": category.name,
+                        "image_url": category.image_url
+                    }
+                for category in menu.category_set.all()]
+            }
+
+        return JsonResponse({"results": results}, status=200)
