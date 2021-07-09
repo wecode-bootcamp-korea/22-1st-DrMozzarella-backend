e8c5a6db19b4563b9b689eba7ab9bf4d0dd2e1a6
Dongmyeong Lee <dom9316@gmail.com>
2021-07-09 23:52:01 +0900

    Fix: set default for cart quantity

--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Cart(models.Model):
     account    = models.ForeignKey('accounts.Account', on_delete=models.CASCADE)
     option     = models.ForeignKey('products.Option', on_delete=models.CASCADE)
-    quantity   = models.IntegerField()
+    quantity   = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
