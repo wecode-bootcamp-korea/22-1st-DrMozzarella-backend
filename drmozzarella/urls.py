bede6a6a51eb2a13611e08b316ecb4ce0ad06c4b
heesu-ahn <anhesu4@gmail.com>
2021-07-09 10:48:33 +0900

    category_view

--- drmozzarella/urls.py
+++ drmozzarella/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
 
+
 urlpatterns = [
+    path("products", include("products.urls"))
 ]
---
+++ products/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from products.views import CategoryView    
+
+urlpatterns = [
+    path('/categories', CategoryView.as_view())
+]
