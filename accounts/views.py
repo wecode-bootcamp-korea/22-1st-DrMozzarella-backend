145d6a65a8ca2da985c9e0a6bc381e1377defd0d
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-07-09 10:59:25 +0900

    Edit: try

--- accounts/views.py
+++ accounts/views.py
@@ -12,19 +12,18 @@ class SignupView(View):
 
         EMAIL_REGES    = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
         PASSWORD_REGES = '[A-Za-z0-9@#$%^&+=]{8,}'
-        
-        if not re.search(EMAIL_REGES, data["email"]):
-            return JsonResponse ({"MESSAGE":"INVALID EMAIL"}, status = 400)
-        
-        if not re.search(PASSWORD_REGES, data["password"]):
-            return JsonResponse ({"MESSAGE": "INVALID PASSWORD"}, status = 400)
-
-        if Account.objects.filter(email=data["email"]).exists():
-            return JsonResponse ({"MESSAGE":"EXISTED EMAIL"}, status = 400)
-        
-        hashed_passwored = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode()
-        
         try:
+            if not re.search(EMAIL_REGES, data["email"]):
+                return JsonResponse ({"MESSAGE":"INVALID EMAIL"}, status = 400)
+            
+            if not re.search(PASSWORD_REGES, data["password"]):
+                return JsonResponse ({"MESSAGE": "INVALID PASSWORD"}, status = 400)
+
+            if Account.objects.filter(email=data["email"]).exists():
+                return JsonResponse ({"MESSAGE":"EXISTED EMAIL"}, status = 400)
+            
+            hashed_passwored = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode()
+        
             Account.objects.create(
                 name     = data["name"],
                 email    = data["email"],
