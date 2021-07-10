22bcc5eb14f4fe0f670a3492fd46a2056d15309a
Dongmyeong Lee <dom9316@gmail.com>
2021-07-10 15:09:43 +0900

    Add: signal handler for Option => Product Comment => Product CommentLike => Comment

--- comments/models.py
+++ comments/models.py
@@ -1,4 +1,8 @@
-from django.db import models
+import re
+from django.db                import models
+from django.patch             import receiver
+from django.db.models.signals import post_save, pre_delete
+from django.db.models         import Avg
 
 class Comment(models.Model):
     account    = models.ForeignKey('accounts.Account', on_delete=models.SET_NULL, null=True)
@@ -21,3 +25,29 @@ class CommentLike(models.Model):
 
     class Meta:
         db_table = 'comment_likes'
+
+@receiver(post_save, sender=Comment)
+def comment_post_save_handler(sender, instance, **kwargs):
+    instance.product.score = instance.product.comment_set.aggregate(Avg('score'))["score__avg"]
+    instance.save()
+
+@receiver(pre_delete, sender=Comment)
+def comment_pre_delete_handler(sender, instance, **kwargs):
+    instance.product.score = instance.product.comment_set.aggreage(Avg('score'))["score__avg"]
+    instance.save()
+
+@receiver(post_save, sender=CommentLike)
+def commentlike_post_save_handler(sender, instance, **kwargs):
+    if instance.flag:
+        instance.comment.like += 1
+    else:
+        instance.comment.dislike += 1
+    instance.comment.save()
+
+@receiver(pre_delete, sender=CommentLike)
+def commentlike_pre_delete_handler(sender, instance, **kwargs):
+    if instance.flag:
+        instance.comment.like -= 1
+    else:
+        instance.comment.dislike -= 1
+    instance.comment.save()
