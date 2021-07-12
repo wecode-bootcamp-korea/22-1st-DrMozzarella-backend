4dc65ee69acae8a07edd2d8ad34404d82fec603e
Dongmyeong Lee <dom9316@gmail.com>
2021-07-12 16:45:13 +0900

    Fix: delete trigger

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,4 @@
 from django.db                import models
-from django.dispatch          import receiver
-from django.db.models.signals import post_save, post_delete
-from django.db.models         import Avg
 
 class Comment(models.Model):
     account    = models.ForeignKey('accounts.Account', on_delete=models.SET_NULL, null=True)
@@ -24,29 +21,3 @@ class CommentLike(models.Model):
 
     class Meta:
         db_table = 'comment_likes'
-
-@receiver(post_save, sender=Comment)
-def comment_post_save_handler(sender, instance, **kwargs):
-    instance.product.score = instance.product.comment_set.aggregate(Avg('score'))["score__avg"]
-    instance.product.save()
-
-@receiver(post_delete, sender=Comment)
-def comment_pre_delete_handler(sender, instance, **kwargs):
-    instance.product.score = instance.product.comment_set.aggreage(Avg('score'))["score__avg"]
-    instance.product.save()
-
-@receiver(post_save, sender=CommentLike)
-def commentlike_post_save_handler(sender, instance, **kwargs):
-    if instance.flag:
-        instance.comment.like += 1
-    else:
-        instance.comment.dislike += 1
-    instance.comment.save()
-
-@receiver(post_delete, sender=CommentLike)
-def commentlike_pre_delete_handler(sender, instance, **kwargs):
-    if instance.flag:
-        instance.comment.like -= 1
-    else:
-        instance.comment.dislike -= 1
-    instance.comment.save()
