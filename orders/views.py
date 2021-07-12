a3f1fbccc7651ffca33e96dfb3ea9feb04723a45
Dongmyeong Lee <dom9316@gmail.com>
2021-07-12 18:29:34 +0900

    wip

--- orders/views.py
+++ orders/views.py
@@ -34,7 +34,9 @@ class CartView(View):
                 "option_id"           : cart.option.id,
                 "weight"              : cart.option.weight,
                 "price"               : cart.option.price,
-                "quantity"            : cart.quantity
+                "quantity"            : cart.quantity,
+                "stocks"              : cart.option.stocks,
+                "availability"        : (cart.option.stocks >= cart.quantity)
             } for cart in carts]
 
         return JsonResponse({"results": results}, status=200)
@@ -77,28 +79,29 @@ class OrderView(View):
     def post(self, request):
         try:
             user_id = 1
-            data  = json.loads(request.body)
             carts = Cart.objects.filter(account_id=user_id)
 
             if carts.exists():
                 order = Order.objects.create(
                     account_id   = user_id,
                     order_number = uuid.uuid4(),
-                    order_status = OrderStatus.objects.get(status_name=data['order_status'])
+                    order_status = OrderStatus.objects.get(status_name="Pending")
                 )
                 
                 for cart in carts:
-                    quantity = min(cart.quantity, cart.option.product.stocks)
+                    if cart.quantity > cart.option.stocks:
+                        order.delete()
+                        return JsonResponse({"messeage": "INVALID_QUANTITY"}, status=400)
 
                     OrderItem.objects.create(
                         order_id    = order.id,
                         option_id   = cart.option.id,
-                        quantity    = quantity,
-                        item_status = ItemStatus.objects.get(status_name=data['item_status'])
+                        quantity    = cart.quantity,
+                        item_status = ItemStatus.objects.get(status_name="Pending")
                     )
-                    cart.option.product.stocks -= quantity
-                    cart.option.product.sales  += quantity
-                    cart.option.product.save()
+                    cart.option.stocks -= cart.quantity
+                    cart.option.sales  += cart.quantity
+                    cart.option.save()
                 
                 carts.delete()
 
@@ -112,11 +115,65 @@ class OrderView(View):
 
     def get(self, request):
         user_id = 1
-        orders = Order.objects.filter(account_id=user_id)
+        orders = reversed(Order.objects.filter(account_id=user_id))
         
         results = [
             {
-                "order_number": order.order_number,
+                "order_id"       : order.id,
+                "order_number"   : order.order_number,
+                "order_status"   : order.order_status.status_name,
+                "ordered_at"     : order.ordered_at,
+                "order_products" : [
+                    {
+                        "order_item_id"     : orderitem.id,
+                        "product_id"        : orderitem.option.product_id,
+                        "product_name"      : orderitem.option.product.name,
+                        "product_image_url" : orderitem.option.product.thumbnail_image_url,
+                        "quantity"          : orderitem.quantity,
+                        "option_id"         : orderitem.option_id,
+                        "weight"            : orderitem.option.weight,
+                        "price"             : orderitem.option.price,
+                        "item_status"       : orderitem.item_status.status_name
+                    } for orderitem in order.orderitem_set.all()]
             } for order in orders]
 
-        return JsonResponse({"results": "message"})
+        return JsonResponse({"results": results}, status=200)
+
+    def put(self, request, order_id):
+        user_id = 1
+        
+        try:
+            data = json.loads(request.body)
+            order = Order.objects.get(id = order_id, account_id = user_id)
+            
+            order.order_status = OrderStatus.objects.get(status_name = data['order_status'])
+            order.save()
+            
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+        
+        except Order.DoesNotExist:
+            return JsonResponse({"message": "INVALID_ORDER_ID"}, status=400)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+    def patch(self, request, order_id, order_item_id):
+        user_id = 1
+
+        try:
+            data = json.loads(request.body)
+            order_item = OrderItem.objects.get(
+                id                = order_item_id,
+                order_id          = order_id,
+                order__account_id = user_id
+            )
+            order_item.item_status = ItemStatus.objects.get(status_name = data['item_status'])
+            order_item.save()
+            
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except OrderItem.DoesNotExist:
+            return JsonResponse({"message": "INVALID_ITEM"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
