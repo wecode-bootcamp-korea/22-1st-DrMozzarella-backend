fc5e8272fd103101da85d05e85bc9a85b441ed48
Dongmyeong Lee <dom9316@gmail.com>
2021-07-13 18:14:54 +0900

    Fix: urls

--- drmozzarella/urls.py
+++ drmozzarella/urls.py
@@ -5,7 +5,8 @@ from products.views import MenuView
 
 urlpatterns = [
     path('menus'    , MenuView.as_view()),
-    path('events', include('events.urls')),
-    path('accounts', include('accounts.urls')),
-    path("products", include("products.urls"))
+    path('events'   , include('events.urls')),
+    path('accounts' , include('accounts.urls')),
+    path('products' , include('products.urls')),
+    path('orders'   , include('orders.urls'))
 ]
