54bf824e5cce9539ce2f9048a1f34f4aa7b44c61
heesu-ahn <anhesu4@gmail.com>
2021-07-14 15:39:57 +0900

    aab

--- products/views.py
+++ products/views.py
@@ -32,16 +32,14 @@ class CategoryView(View):
         try:
             category = Category.objects.get(id = category_id)
             results = {"category_id"           : category.id,
-                        "category_name"         : category.name,
-                        "category_description"  : category.description,
-                        "category_image_url"    : category.image_url
+                       "category_name"         : category.name,
+                       "category_description"  : category.description,
+                       "category_image_url"    : category.image_url
             }   
             return JsonResponse({"results": results }, status=200)
 
         except Category.DoesNotExist:
             return JsonResponse({"message": "INVALID_CATEGORY_ID"}, status=404)
-
-
 
 class ProductsView(View) :
     def get(self,request) :
