ae2e33bb1f3e3b743fce71b64628e78883a10d13
Dongmyeong Lee <dom9316@gmail.com>
2021-07-15 21:23:35 +0900

    Fix: convention

--- products/views.py
+++ products/views.py
@@ -105,96 +105,100 @@ class ProductsView(View) :
 
 class ProductDetailView(View):
     def get(self, request, product_id):
-        current_product = Product.objects.select_related('nutrition')\
-            .prefetch_related('category', 'option_set', 'image_set', 'category__menu')\
-            .get(id=product_id)
+        try:
+            current_product = Product.objects.select_related('nutrition')\
+                .prefetch_related('category', 'option_set', 'image_set', 'category__menu')\
+                .get(id=product_id)
 
-        current_category_dict = {}
-        
-        for category in current_product.category.all():
-            current_category_dict[category.menu.name] = category
+            current_category_dict = {}
+            
+            for category in current_product.category.all():
+                current_category_dict[category.menu.name] = category
 
-        routine_products = [current_product]
-        compare_products = [current_product]
+            routine_products = [current_product]
+            compare_products = [current_product]
 
-        routine_products.extend(list(Product.objects\
-                                     .filter(category=current_category_dict["milk"])\
-                                     .exclude(category=current_category_dict["style"])\
-                                     .prefetch_related('category', 'option_set')))
+            routine_products.extend(list(Product.objects\
+                                         .filter(category=current_category_dict["milk"])\
+                                         .exclude(category=current_category_dict["style"])\
+                                         .prefetch_related('category', 'option_set')))
 
-        compare_products.extend(list(Product.objects\
-                                     .filter(category=current_category_dict["milk"])\
-                                     .filter(category=current_category_dict["style"])\
-                                     .exclude(category=current_category_dict["countries"])))
-                            
+            compare_products.extend(list(Product.objects\
+                                         .filter(category=current_category_dict["milk"])\
+                                         .filter(category=current_category_dict["style"])\
+                                         .exclude(category=current_category_dict["countries"])))
+                                
 
-        product_result = {
-            'product_id'       : current_product.id,
-            'product_name'     : current_product.name,
-            'summary'          : current_product.summary,
-            'description'      : current_product.description,
-            'score'            : current_product.score,
-            'description_image': current_product.description_image_url,
-            'image_urls'       : [ image.image_url for image in current_product.image_set.all() ],
-            'categories'       : [
+            product_result = {
+                'product_id'       : current_product.id,
+                'product_name'     : current_product.name,
+                'summary'          : current_product.summary,
+                'description'      : current_product.description,
+                'score'            : current_product.score,
+                'description_image': current_product.description_image_url,
+                'image_urls'       : [ image.image_url for image in current_product.image_set.all() ],
+                'categories'       : [
+                    {
+                        "category_id"         : category.id,
+                        "category_name"       : category.name,
+                        "category_image_url"  : category.image_url,
+                        "category_description": category.description
+                    } for category in current_product.category.all()
+                ],
+                'option' : [
+                    {
+                        "price"     : option.price,
+                        "weight"    : option.weight,
+                        "option_id" : option.id
+                    } for option in current_product.option_set.all()
+                ],
+                'nutrition' : [
+                    {
+                        field.name : field.value_from_object(current_product.nutrition)
+                    } for field in current_product.nutrition._meta.fields if field.name != "id"
+                ]
+            }
+
+            routine_results = [
                 {
-                    "category_id"         : category.id,
-                    "category_name"       : category.name,
-                    "category_image_url"  : category.image_url,
-                    "category_description": category.description
-                } for category in current_product.category.all()
-            ],
-            'option' : [
-                {
-                    "price"     : option.price,
-                    "weight"    : option.weight,
-                    "option_id" : option.id
-                } for option in current_product.option_set.all()
-            ],
-            'nutrition' : [
-                {
-                    field.name : field.value_from_object(current_product.nutrition)
-                } for field in current_product.nutrition._meta.fields if field.name != "id"
+                    'product_id'     : product.id,
+                    'product_name'   : product.name,
+                    'score'          : product.score,
+                    'thumbnail_image': product.thumbnail_image_url,
+                    'hover_image'    : product.hover_image_url,
+                    'current'        : (product == current_product),
+                    'option' : [
+                        {
+                            "price"     : option.price,
+                            "weight"    : option.weight,
+                            "option_id" : option.id
+                        } for option in product.option_set.all()
+                    ]
+                } for product in routine_products[:3]
             ]
-        }
 
-        routine_results = [
-            {
-                'product_id'     : product.id,
-                'product_name'   : product.name,
-                'score'          : product.score,
-                'thumbnail_image': product.thumbnail_image_url,
-                'hover_image'    : product.hover_image_url,
-                'current'        : (product == current_product),
-                'option' : [
-                    {
-                        "price"     : option.price,
-                        "weight"    : option.weight,
-                        "option_id" : option.id
-                    } for option in product.option_set.all()
-                ]
-            } for product in routine_products[:3]
-        ]
+            compare_results  = [
+                {
+                    'product_name'   : product.name,
+                    'description'    : product.description,
+                    'thumbnail_image': product.thumbnail_image_url,
+                    'option' : [
+                        {
+                            "price"     : option.price,
+                            "weight"    : option.weight,
+                            "option_id" : option.id
+                        } for option in product.option_set.all()
+                    ]
+                } for product in compare_products[:3]
+            ]
 
-        compare_results  = [
-            {
-                'product_name'   : product.name,
-                'description'    : product.description,
-                'thumbnail_image': product.thumbnail_image_url,
-                'option' : [
-                    {
-                        "price"     : option.price,
-                        "weight"    : option.weight,
-                        "option_id" : option.id
-                    } for option in product.option_set.all()
-                ]
-            } for product in compare_products[:3]
-        ]
+            results = {
+                "product" : product_result,
+                "routine" : routine_results,
+                "compare" : compare_results
+            }
 
-        results = {
-            "product" : product_result,
-            "routine" : routine_results,
-            "compare" : compare_results
-        }
+            return JsonResponse ({"results": results}, status = 200)
 
-        return JsonResponse ({"results": results}, status = 200)
+        except Product.DoesNotExist:
+            return JsonResponse({"message": "INVALID_PRODUCT"}, stauts=400)
